65921872336a49b33e9deb23b3e783177f09557d
Aaron Rivas <aarivas793@gmail.com>
2024-12-21 12:15:15 -0800

    created copy function to copy src to dest file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-from textnode import TextNode, TextType, text_node_to_html_node
-from htmlnode import HTMLNode, LeafNode, ParentNode
+import os, shutil
+source = "./static"
+destination = "./public"
 
 def main():
-  text_node = TextNode("hello world!", TextType.BOLD, "https://www.google.com")
-  print(text_node_to_html_node(text_node))
+  if os.path.exists(destination):
+    shutil.rmtree(destination)
+  copy_files_recursive(source, destination)
   
+def copy_files_recursive(source_dir_path, dest_dir_path):
+  if not os.path.exists(dest_dir_path):
+    os.mkdir(dest_dir_path)
+
+  for filename in os.listdir(source_dir_path):
+    from_path = os.path.join(source_dir_path, filename)
+    dest_path = os.path.join(dest_dir_path, filename)
+    print(f" * {from_path} -> {dest_path}")
+    if os.path.isfile(from_path):
+      shutil.copy(from_path, dest_path)
+    else:
+      copy_files_recursive(from_path, dest_path)
 
 main()
